d9b4f55bf1296e287db9bca7cfc441baf07b32e2
aziz krayyem <azizkrayyem7@gmail.com>
2022-04-11 22:52:52 +0300

    add Post& Get APIView

--- core/views.py
+++ core/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-# Create your views here.
+from .models import Post
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .serializers import PostSerializer
+# Create your views here.
 
 class TestView(APIView):
     def get(self,request, *args, **kwargs):
-        data={
-            'name':'aziz',
-            'age':23
-        }
-        return Response(data)
+        qs = Post.objects.all()
+        serializer = PostSerializer(qs, many=True)
+        return Response(serializer.data)
+    
+    def post(self,request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
